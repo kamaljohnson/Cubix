1a7a41ae21f48f84d6d50d2e42767f9e9bcb0332
Kamal Johnson <kamaljohnson12345@gmail.com>
2018-01-26 08:35:38 +0530

    Add files via upload

---
+++ PlayerControl.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    Rigidbody player;
+    float speed;
+    Vector3 destination;    //the next destination
+    //the individual colliders for the player 
+    public GameObject rightCollider;
+    public GameObject leftCollider;
+    public GameObject forwardCollider;
+    public GameObject backCollider;
+
+    private triggering rightTrig;
+    private triggering leftTrig;
+    private triggering forwardTrig;
+    private triggering backTrig;
+
+    enum direction  //for the direcitons 
+    {
+        None,
+        Right,
+        Left,
+        Forward,
+        Back
+    };
+
+    direction currentDireciton;
+    bool inMotion;
+    bool inJunction;
+    bool atBoundary;
+    void Start () {
+
+        speed = 0.1f;
+        rightTrig = rightCollider.GetComponent<triggering>();
+        leftTrig = leftCollider.GetComponent<triggering>();
+        forwardTrig = forwardCollider.GetComponent<triggering>();
+        backTrig = backCollider.GetComponent<triggering>();
+
+        currentDireciton = direction.None;
+        player = GetComponent<Rigidbody>();
+        inMotion = false;
+        atBoundary = false;
+        inJunction = true;
+	}
+	
+	void Update () {
+
+        TrigCol();
+
+        if(atBoundary)
+        {
+            changePlane();
+        }
+        if(inJunction)
+        {
+            changeDirection();
+        }
+
+        Move();
+	}
+    void TrigCol()  //manupulating the data of the triggered colliders RLFB
+    {
+        Debug.Log(rightTrig.trig + " right");
+        Debug.Log(leftTrig.trig + " left");
+        Debug.Log(forwardTrig.trig + " forward");
+        Debug.Log(backTrig.trig + " back");
+    }
+    void Move() //this funtion will controll the movement on the maze plane 
+    {
+        Debug.Log("moving");
+        switch(currentDireciton)
+        {
+            case direction.Right:
+                destination = Vector3.right;
+                break;
+            case direction.Left:
+                destination = Vector3.left;
+                break;
+            case direction.Forward:
+                destination = Vector3.forward;
+                break;
+            case direction.Back:
+                destination = Vector3.back;
+                break;
+
+        }
+        player.MovePosition(player.position + destination*speed);
+        
+    }
+    void changeDirection()  //used to change the direciton of the player
+    {
+        Debug.Log("changing direciton");
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            Debug.Log("Right");
+            currentDireciton = direction.Right;
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            Debug.Log("Left");
+            currentDireciton = direction.Left;
+        }
+        else if (Input.GetAxis("Vertical") > 0)
+        {
+            Debug.Log("Forward");
+            currentDireciton = direction.Forward;
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            Debug.Log("Back");
+            currentDireciton = direction.Back;
+        }
+        else
+        {
+            currentDireciton = direction.None;
+        }
+    }
+    void changePlane()  //for changing the current face of the maze 
+    {
+        Debug.Log("Changing plane");
+    }
+}
---
+++ triggering.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggering : MonoBehaviour {
+
+    public bool trig;
+    void OntriggerEnter(Collider other)
+    {
+        Debug.Log("Triggered");
+        trig = true;
+    }
+    void OntriggerExit(Collider other)
+    {
+        Debug.Log("hello");
+        trig = false;
+    }
+
+}
